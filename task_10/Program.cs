bec2f271df100e7a7ba9c8f5f471803e48709eb6
Алексей Апекишев <apekishev1@mail.ru>
2023-06-16 08:44:02 +0300

    сделана 10-я задача

---
+++ task_10/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.Write("Enter a number =>  ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"second number {(number % 100)/10}");
