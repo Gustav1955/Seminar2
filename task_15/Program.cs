5f641ca6c902e7f6044eaa8d9a7c98874007ff81
Алексей Апекишев <apekishev1@mail.ru>
2023-06-19 21:01:37 +0300

    сделано 15-е задание

---
+++ task_15/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую 
+день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.Write("Введите числo =>  ");
+int n = Convert.ToInt32(Console.ReadLine());
+Dictionary<int, string> map = new Dictionary<int, string>() /*считываем словарь*/
+{
+    { 1, "Pn"},
+    { 2, "Vt"},
+    { 3, "Sr"},
+    { 4, "Cht"},
+    { 5, "Pt"},
+    { 6, "Sb"},
+    { 7, "Vs"},
+};
+if (n > 0 && n < 6) Console.WriteLine("No");
+if (n > 5 && n < 8) Console.WriteLine("Yes");
+if (n < 1 || n > 7) Console.WriteLine("Нет такого дня недели");
+
